312fb289f52a34289cd1c0eb4d7b91cd64358e28
yonosuke <ysk@d-suger.com>
2012-08-29 19:15:56 +0900

    0829

---
+++ 0829/Ch10/Ch10_01.java
@@ -0,0 +1,14 @@
+package Ch10;
+
+public class Ch10_01 {
+	int sum(int i, int j) {
+		return  i + j;
+	}
+
+	public static void main(String[] args) {
+		Ch10_01 calc = new Ch10_01();
+		System.out.println(calc.sum(1, 2));
+
+	}
+}
+
