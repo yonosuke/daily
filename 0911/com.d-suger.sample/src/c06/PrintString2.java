96100bbaf0b91a721001bf4ad52e67d079acd742
yonosuke <ysk@d-suger.com>
2012-09-11 15:36:39 +0900

    0911

---
+++ 0911/com.d-suger.sample/src/c06/PrintString2.java
@@ -0,0 +1,22 @@
+package c06;
+
+public class PrintString2 {
+	String str = "String";
+	void printStr() {
+		System.out.println(str);
+	}
+	
+	void printStr2() {
+		printStr();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+
+	}
+
+}
